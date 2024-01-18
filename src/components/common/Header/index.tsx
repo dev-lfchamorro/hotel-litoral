1b19b11faea5c9fd0cc6a0ff518f954d0568d8bc
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-17 20:13:28 -0500

    Header basic component was created

---
+++ src/components/common/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <li>Inicio</li>
+        <li>Sobre nós</li>
+        <li>Quartos</li>
+        <li>Contato</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
