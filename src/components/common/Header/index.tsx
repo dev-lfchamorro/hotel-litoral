b148baca1596b522e8fa97efd249cf9f3eb576e2
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-18 22:59:06 -0500

    Initial style for the header menu was created

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -4,22 +4,30 @@ import "./styles.scss";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <ul>
-        <li>
-          <Link to={paths.home}>Inicio</Link>
-        </li>
-        <li>
-          <Link to={paths.aboutUs}>Sobre nós</Link>
-        </li>
-        <li>
-          <Link to={paths.rooms}>Quartos</Link>
-        </li>
-        <li>
-          <Link to={paths.contact}>Contato</Link>
-        </li>
-      </ul>
-    </div>
+    <header id="header-container">
+      <div className="logo">
+        <div className="logo-name">
+          Hotel <span>Litoral</span>
+        </div>
+        <span className="logo-slogan">Espaço, Conforto e Tranquilidade</span>
+      </div>
+      <nav className="nav-container">
+        <ul className="nav-list">
+          <li>
+            <Link to={paths.home}>Inicio</Link>
+          </li>
+          <li>
+            <Link to={paths.aboutUs}>Sobre nós</Link>
+          </li>
+          <li>
+            <Link to={paths.rooms}>Quartos</Link>
+          </li>
+          <li>
+            <Link to={paths.contact}>Contato</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
