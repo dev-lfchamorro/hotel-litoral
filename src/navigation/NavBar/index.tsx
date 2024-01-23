bb33a94f9ad1d3142503dfa9303098115f563ea9
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-22 21:05:11 -0500

    NavBar component was refactored and implemented the MenuOPtions component

--- src/navigation/NavBar/index.tsx
+++ src/navigation/NavBar/index.tsx
@@ -1,29 +1,10 @@
 import React, { useState } from "react";
-import { NavLink, NavLinkProps } from "react-router-dom";
 import IconList from "../../assets/icons/list.svg";
 import IconX from "../../assets/icons/x.svg";
 import Icon from "../../components/Icon";
 import { IconSize } from "../../components/Icon/enums";
-import { paths } from "../../constants";
+import MenuOptions from "../MenuOptions";
 import "./styles.scss";
-
-interface CustomNavLinkProps extends NavLinkProps {
-  activeClassName?: string;
-}
-
-// TODO: This component is to fix the type error with TypeScript about the activeClassName property
-const CustomNavLink: React.FC<CustomNavLinkProps> = ({
-  to,
-  children,
-  activeClassName,
-  ...rest
-}) => {
-  return (
-    <NavLink to={to} className={activeClassName} {...rest}>
-      {children}
-    </NavLink>
-  );
-};
 
 const NavBar: React.FC = () => {
   const [visibleMenu, setVisibleMenu] = useState("");
@@ -50,20 +31,7 @@ const NavBar: React.FC = () => {
           size={IconSize.xl}
         />
 
-        <ul className="nav-list">
-          <li>
-            <CustomNavLink to={paths.home}>Inicio</CustomNavLink>
-          </li>
-          <li>
-            <CustomNavLink to={paths.aboutUs}>Sobre nós</CustomNavLink>
-          </li>
-          <li>
-            <CustomNavLink to={paths.rooms}>Quartos</CustomNavLink>
-          </li>
-          <li>
-            <CustomNavLink to={paths.contact}>Contato</CustomNavLink>
-          </li>
-        </ul>
+        <MenuOptions />
       </nav>
     </>
   );
