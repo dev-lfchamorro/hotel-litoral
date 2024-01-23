56e63394a428e08b83510c3c4af097bcc06c0728
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-22 22:26:04 -0500

    Main styles ans logic structure for Button component was added

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,9 +3,17 @@ import "./styles.scss";
 import { ButtonProps } from "./types";
 
 const Button: React.FC<ButtonProps> = ({ iconPath, text, onClick }) => {
+  const cursorType = onClick ? "cursor-pointer" : "";
+
   return (
-    <button className="button" onClick={onClick}>
-      {iconPath && <img src={iconPath} alt="Icon" />}
+    <button className={`button ${cursorType}`} onClick={onClick}>
+      {iconPath && (
+        <img
+          src={iconPath}
+          alt="Icon"
+          style={{ filter: "brightness(0) invert(1)" }}
+        />
+      )}
       {text}
     </button>
   );
