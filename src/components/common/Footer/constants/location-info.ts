26090f71a596a26a93e446e5170e10959abff4fa
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-27 00:02:25 -0500

    New icons were implemented in constants for Footer component

--- src/components/common/Footer/constants/location-info.ts
+++ src/components/common/Footer/constants/location-info.ts
@@ -1,13 +1,15 @@
-import IconPhone from "../../../../assets/icons/telephone.svg";
+import IconBrazil from "../../../../assets/img/brazil-48.png";
+import IconElevator from "../../../../assets/img/elevator-50.png";
+import IconPool from "../../../../assets/img/pool-50.png";
 import { ListItemType } from "../../../../types";
 
 export const LocationInfo: ListItemType[] = [
   { textItem: "Av.Oceânica, 819" },
   { textItem: "Atalaia - Aracaju/SE" },
-  { textItem: "Brasil", iconItem: IconPhone },
+  { textItem: "Brasil", iconItem: IconBrazil },
 ];
 
 export const WeDoNotOwnInfo: ListItemType[] = [
-  { textItem: "Elevador", iconItem: IconPhone },
-  { textItem: "Piscina", iconItem: IconPhone },
+  { textItem: "Elevador", iconItem: IconElevator },
+  { textItem: "Piscina", iconItem: IconPool },
 ];
--- src/components/common/Footer/constants/we-offer-info.ts
+++ src/components/common/Footer/constants/we-offer-info.ts
@@ -1,20 +1,28 @@
-import IconPhone from "../../../../assets/icons/telephone.svg";
+import IconCupHot from "../../../../assets/icons/cup-hot.svg";
+import IconTv from "../../../../assets/icons/tv.svg";
+import IconWifi from "../../../../assets/icons/wifi.svg";
+import IconAir from "../../../../assets/img/air-50.png";
+import IconBed from "../../../../assets/img/bed-50.png";
+import IconCradle from "../../../../assets/img/cradle-64.png";
+import IconFridge from "../../../../assets/img/fridge-64.png";
+import IconGarage from "../../../../assets/img/garage-50.png";
+import IconHairDryer from "../../../../assets/img/hairdryer-64.png";
 import { ListItemType } from "../../../../types";
 
 export const WeOfferInfo: ListItemType[] = [
   {
     textItem: "TV de LED com canais por assinatura",
-    iconItem: IconPhone,
+    iconItem: IconTv,
   },
-  { textItem: "WI-FI", iconItem: IconPhone },
-  { textItem: "Ótimo café da manhã (incluso)", iconItem: IconPhone },
-  { textItem: "Ar condicionado split", iconItem: IconPhone },
-  { textItem: "Garagem fechada - GRATUITA", iconItem: IconPhone },
+  { textItem: "WI-FI", iconItem: IconWifi },
+  { textItem: "Ótimo café da manhã (incluso)", iconItem: IconCupHot },
+  { textItem: "Ar-condicionado split", iconItem: IconAir },
+  { textItem: "Garagem fechada - GRATUITA", iconItem: IconGarage },
   {
     textItem: "Secador de cabelo em todos os aptos",
-    iconItem: IconPhone,
+    iconItem: IconHairDryer,
   },
-  { textItem: "Berço (apenas 1)", iconItem: IconPhone },
-  { textItem: "Frigobar com preços acessíveis", iconItem: IconPhone },
-  { textItem: "Cama box", iconItem: IconPhone },
+  { textItem: "Berço (apenas 1)", iconItem: IconCradle },
+  { textItem: "Frigobar com preços acessíveis", iconItem: IconFridge },
+  { textItem: "Cama box", iconItem: IconBed },
 ];
