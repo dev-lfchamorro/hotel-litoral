40d0f87a0ede0676d31e8fcf540496ce912d9550
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-28 20:09:21 -0500

    Constant for gallery images were created

---
+++ src/components/ImgCarousel/constants/images.ts
@@ -0,0 +1,37 @@
+import Image1 from "../../../assets/img/gallery/aisle.jpeg";
+import Image2 from "../../../assets/img/gallery/balcony.jpeg";
+import Image3 from "../../../assets/img/gallery/bathroom.jpeg";
+import Image4 from "../../../assets/img/gallery/dinningroom.jpg";
+import Image5 from "../../../assets/img/gallery/garage.jpeg";
+import Image6 from "../../../assets/img/gallery/garden.jpeg";
+import Image7 from "../../../assets/img/gallery/kitchen.jpg";
+import Image8 from "../../../assets/img/gallery/livingroom.jpg";
+import Image9 from "../../../assets/img/gallery/main-facade.jpeg";
+import Image10 from "../../../assets/img/gallery/reception.jpeg";
+import Image11 from "../../../assets/img/gallery/reception-2.jpeg";
+import Image12 from "../../../assets/img/gallery/reception-3.jpeg";
+import Image13 from "../../../assets/img/gallery/room-1.jpeg";
+import Image14 from "../../../assets/img/gallery/room-2.jpeg";
+import Image15 from "../../../assets/img/gallery/room-3.jpeg";
+import Image16 from "../../../assets/img/gallery/room-4.jpeg";
+import Image17 from "../../../assets/img/gallery/room-5.jpeg";
+
+export const galleryImages = [
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Image7,
+  Image8,
+  Image9,
+  Image10,
+  Image11,
+  Image12,
+  Image13,
+  Image14,
+  Image15,
+  Image16,
+  Image17,
+];
