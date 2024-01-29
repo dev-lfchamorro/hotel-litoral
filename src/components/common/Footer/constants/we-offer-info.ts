09f03906c65b59a2c80cea1cd4436faade79ab04
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-28 20:08:02 -0500

    Path directs for img icons were fixed

--- src/components/common/Footer/constants/we-offer-info.ts
+++ src/components/common/Footer/constants/we-offer-info.ts
@@ -1,12 +1,12 @@
 import IconCupHot from "../../../../assets/icons/cup-hot.svg";
 import IconTv from "../../../../assets/icons/tv.svg";
 import IconWifi from "../../../../assets/icons/wifi.svg";
-import IconAir from "../../../../assets/img/air-50.png";
-import IconBed from "../../../../assets/img/bed-50.png";
-import IconCradle from "../../../../assets/img/cradle-64.png";
-import IconFridge from "../../../../assets/img/fridge-64.png";
-import IconGarage from "../../../../assets/img/garage-50.png";
-import IconHairDryer from "../../../../assets/img/hairdryer-64.png";
+import IconAir from "../../../../assets/img/icons/air-50.png";
+import IconBed from "../../../../assets/img/icons/bed-50.png";
+import IconCradle from "../../../../assets/img/icons/cradle-64.png";
+import IconFridge from "../../../../assets/img/icons/fridge-64.png";
+import IconGarage from "../../../../assets/img/icons/garage-50.png";
+import IconHairDryer from "../../../../assets/img/icons/hairdryer-64.png";
 import { ListItemType } from "../../../../types";
 
 export const WeOfferInfo: ListItemType[] = [
