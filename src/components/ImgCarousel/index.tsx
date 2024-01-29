f74d0b34137316059c1cada465e1bb99f8c0b53c
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-28 22:41:27 -0500

    ImageModal component was implemented and logic to pass images was added

--- src/components/ImgCarousel/index.tsx
+++ src/components/ImgCarousel/index.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { getImgNameByPath } from "../../helpers";
+import ImageModal from "../ImageModal";
 import { galleryImages } from "./constants";
 import "./styles.scss";
 
 const ImgCarousel: React.FC = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  const handleOpenModal = (idx: number) => {
+    setSelectedImageIndex(idx);
+    setIsOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+  };
+
+  const handleNextImage = () => {
+    setSelectedImageIndex((prevIndex) =>
+      prevIndex === galleryImages.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevImage = () => {
+    setSelectedImageIndex((prevIndex) =>
+      prevIndex === 0 ? galleryImages.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="gallery-container">
       <div className="gallery-wrapper">
@@ -12,9 +37,27 @@ const ImgCarousel: React.FC = () => {
             const altAttribute = getImgNameByPath(img);
 
             return (
-              <img src={img} alt={altAttribute} loading="eager" key={idx + 1} />
+              <img
+                alt={altAttribute}
+                className="gallery-img"
+                key={idx + 1}
+                loading="eager"
+                src={img}
+                onClick={() => handleOpenModal(idx)}
+              />
             );
           })}
+
+          {isOpenModal && (
+            <ImageModal
+              imgUrl={galleryImages[selectedImageIndex]}
+              currentImg={selectedImageIndex + 1}
+              totalImg={galleryImages.length}
+              onClose={handleCloseModal}
+              onNext={handleNextImage}
+              onPrev={handlePrevImage}
+            />
+          )}
         </div>
       </div>
     </div>
