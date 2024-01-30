689c46f78222360b9f4666490af51d98aa197c76
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-29 21:27:17 -0500

    section-text type was created

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./list-item-type";
 export * from "./menu-options";
 export * from "./routes-config";
+export * from "./section-text";
---
+++ src/types/section-text.ts
@@ -0,0 +1,4 @@
+export type SectionText = {
+  description: string;
+  title: string;
+};
