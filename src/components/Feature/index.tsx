3c95702e9d2d5adf9a4c0770ee9bd2c1ea19c5b7
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-29 21:05:00 -0500

    Refactor was made and TextContent was implemented to re-use components

--- src/components/Feature/index.tsx
+++ src/components/Feature/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TextContent from "../TextContent";
 import "./styles.scss";
 import { FeatureProps } from "./types";
 
@@ -9,9 +10,9 @@ const Feature: React.FC<{ features: FeatureProps[] }> = ({ features }) => {
         <div className="feature-element" key={idx + 1}>
           <feature.icon className="feature-img" />
 
-          <span className="feature-title">{feature.title}</span>
-
-          <span className="feature-description">{feature.description}</span>
+          <TextContent orientation="center" size="medium" title={feature.title}>
+            {feature.description}
+          </TextContent>
         </div>
       ))}
     </div>
