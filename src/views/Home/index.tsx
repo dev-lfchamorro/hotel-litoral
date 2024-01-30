aa2dad66edd29271ff76d414481b352211035777
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-29 20:28:11 -0500

    TextContent component was implemented on Home component

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Feature from "../../components/Feature";
 import ImgCarousel from "../../components/ImgCarousel";
+import TextContent from "../../components/TextContent";
 import Footer from "../../components/common/Footer";
 import Header from "../../components/common/Header";
 import { features } from "./constants";
@@ -12,6 +13,18 @@ const Home: React.FC = () => {
       <Header />
       <Feature features={features} />
       <ImgCarousel />
+      <TextContent
+        orientation="center"
+        size="large"
+        title="Hotel Litoral / Desde o 2002"
+      >
+        Desde o 2002, oferecemos o melhor serviço possível a turistas
+        brasileiros e estrangeiros, localizado a apenas 100 metros da praia, com
+        excelente qualidade humana e instalações simples, mas muito confortáveis
+        ​​e atenciosas; cuidamos para que você se sinta confortável em nosso
+        hotel, oferecemos um excelente café da manhã, onde você pode comer
+        confortavelmente e começar o dia com a melhor atitude.
+      </TextContent>
       <Footer />
     </div>
   );
