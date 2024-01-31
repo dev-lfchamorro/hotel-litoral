aaf69c5d9c6666b6f605e6edd5e4caf8a670114c
Luis Fernando <dev.lfchamorro@gmail.com>
2024-01-30 21:44:45 -0500

    SectionImageText component was implemented in Home component view

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import ImgSection1 from "../../assets/img/gallery/main-facade.jpeg";
 import Feature from "../../components/Feature";
 import ImgCarousel from "../../components/ImgCarousel";
+import SectionImageText from "../../components/SectionImageText";
 import TextContent from "../../components/TextContent";
 import Footer from "../../components/common/Footer";
 import Header from "../../components/common/Header";
-import { homeSectionsText } from "../../types";
+import { SectionsText } from "../../types";
 import { features } from "./constants";
 import "./styles.scss";
 
 const Home: React.FC = () => {
-  const [section1, section2] = homeSectionsText.Inicio || [];
+  const [section1, section2] = SectionsText.Inicio || [];
 
   return (
     <div className="home-container">
@@ -17,17 +19,20 @@ const Home: React.FC = () => {
       <Feature features={features} />
       <ImgCarousel />
 
-      {homeSectionsText.Inicio && (
+      <SectionImageText
+        imageSize="middle"
+        img={ImgSection1}
+        sectionOrientation="right"
+        textContentOrientation="middle"
+      >
         <TextContent orientation="left" size="medium" title={section1.title}>
           {section1.description}
         </TextContent>
-      )}
+      </SectionImageText>
 
-      {homeSectionsText.Inicio && (
-        <TextContent orientation="center" size="medium" title={section2.title}>
-          {section2.description}
-        </TextContent>
-      )}
+      <TextContent orientation="center" size="medium" title={section2.title}>
+        {section2.description}
+      </TextContent>
 
       <Footer />
     </div>
