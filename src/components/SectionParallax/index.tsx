dd69482d7b24d52ec886e7e26c57da1c594830dd
Luis Fernando <dev.lfchamorro@gmail.com>
2024-02-07 21:29:40 -0500

    Installed packages were implemented on SectionParallax component and refactor was added

--- src/components/SectionParallax/index.tsx
+++ src/components/SectionParallax/index.tsx
@@ -1,15 +1,21 @@
+import "animate.css";
 import React from "react";
+import { useInView } from "react-intersection-observer";
 import "./styles.scss";
 import { SectionParallaxProps } from "./types";
 
 const SectionParallax: React.FC<SectionParallaxProps> = ({ children, img }) => {
-  console.log(img);
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-100px 0px",
+  });
+
+  const animateCssClass = inView ? "animate__animated animate__backInUp" : "";
+
   return (
-    <div
-      className="section-parallax-container"
-      style={{ backgroundImage: `url(${img})` }}
-    >
-      {children}
+    <div className="section-parallax-container" ref={ref}>
+      <div className="parallax-bg" style={{ backgroundImage: `url(${img})` }} />
+      <div className={`parallax-content ${animateCssClass}`}>{children}</div>
     </div>
   );
 };
