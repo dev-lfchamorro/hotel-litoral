7318ae2ac782f8cbe51ef9afad2bbf175e4c2d39
Luis Fernando <dev.lfchamorro@gmail.com>
2024-02-16 21:37:10 -0500

    framer-motion was implemented on ImgCarousel to add dynamic drag to Images

--- src/components/ImgCarousel/index.tsx
+++ src/components/ImgCarousel/index.tsx
@@ -1,5 +1,8 @@
+import { motion } from "framer-motion";
 import React, { useState } from "react";
+import { ReactComponent as IconInfo } from "../../assets/icons/info-circle.svg";
 import { getImgNameByPath } from "../../helpers";
+import { useSlider } from "../../hooks";
 import ImageModal from "../ImageModal";
 import { galleryImages } from "./constants";
 import "./styles.scss";
@@ -8,14 +11,14 @@ const ImgCarousel: React.FC = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
+  const { carouselRef, width } = useSlider();
+
   const handleOpenModal = (idx: number) => {
     setSelectedImageIndex(idx);
     setIsOpenModal(true);
   };
 
-  const handleCloseModal = () => {
-    setIsOpenModal(false);
-  };
+  const handleCloseModal = () => setIsOpenModal(false);
 
   const handleNextImage = () => {
     setSelectedImageIndex((prevIndex) =>
@@ -31,36 +34,53 @@ const ImgCarousel: React.FC = () => {
 
   return (
     <div className="img-carousel-container">
-      <div className="img-carousel-wrapper">
-        <div className="gallery">
+      <motion.div
+        className="img-carousel-wrapper"
+        ref={carouselRef}
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          className="gallery"
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          dragElastic={0.3}
+        >
           {galleryImages.map((img, idx) => {
             const altAttribute = getImgNameByPath(img);
 
             return (
-              <img
-                alt={altAttribute}
-                className="img-carousel-img"
+              <motion.div
                 key={idx + 1}
-                loading="eager"
-                src={img}
-                onClick={() => handleOpenModal(idx)}
-              />
+                onDoubleClick={() => handleOpenModal(idx)}
+              >
+                <img
+                  alt={altAttribute}
+                  className="img-carousel-img"
+                  loading="eager"
+                  src={img}
+                />
+              </motion.div>
             );
           })}
+        </motion.div>
 
-          {isOpenModal && (
-            <ImageModal
-              currentImg={selectedImageIndex + 1}
-              imgUrl={galleryImages[selectedImageIndex]}
-              isOpen={isOpenModal}
-              totalImg={galleryImages.length}
-              onClose={handleCloseModal}
-              onNext={handleNextImage}
-              onPrev={handlePrevImage}
-            />
-          )}
-        </div>
-      </div>
+        {isOpenModal && (
+          <ImageModal
+            currentImg={selectedImageIndex + 1}
+            imgUrl={galleryImages[selectedImageIndex]}
+            isOpen={isOpenModal}
+            totalImg={galleryImages.length}
+            onClose={handleCloseModal}
+            onNext={handleNextImage}
+            onPrev={handlePrevImage}
+          />
+        )}
+      </motion.div>
+
+      <span className="info-text">
+        <IconInfo />
+        Para ampliar a imagem, clique duas vezes nela
+      </span>
     </div>
   );
 };
