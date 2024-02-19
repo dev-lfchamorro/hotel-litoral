4ad12dc720f10b4a4da3936a429301d3669bf48a
Luis Fernando <dev.lfchamorro@gmail.com>
2024-02-18 20:57:32 -0500

    Slider component was refactored in order to receive a children dynamic prop

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,18 +1,10 @@
 import { motion } from "framer-motion";
 import React from "react";
-import ImgRoom1 from "../../assets/img/gallery/room-1.jpeg";
-import ImgRoom2 from "../../assets/img/gallery/room-2.jpeg";
-import ImgRoom3 from "../../assets/img/gallery/room-3.jpeg";
-import ImgRoom4 from "../../assets/img/gallery/room-4.jpeg";
-import ImgRoom5 from "../../assets/img/gallery/room-5.jpeg";
-import { getImgNameByPath } from "../../helpers";
 import { useSlider } from "../../hooks";
-import Button from "../Button";
 import "./styles.scss";
+import { SliderProps } from "./types";
 
-const images = [ImgRoom1, ImgRoom2, ImgRoom3, ImgRoom4, ImgRoom5];
-
-const Slider: React.FC = () => {
+const Slider: React.FC<SliderProps> = ({ children }) => {
   const { carouselRef, width } = useSlider();
 
   return (
@@ -27,12 +19,7 @@ const Slider: React.FC = () => {
         dragConstraints={{ right: 0, left: -width }}
         dragElastic={0.3}
       >
-        {images.map((img, idx) => (
-          <motion.div className="item" key={idx}>
-            <img src={img} alt={getImgNameByPath(img)} />
-            <Button text="Apto. Single" className="button-room" />
-          </motion.div>
-        ))}
+        {children.map((element) => element)}
       </motion.div>
     </motion.div>
   );
