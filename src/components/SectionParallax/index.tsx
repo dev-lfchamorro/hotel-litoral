321daf5a3647e92f7b3ff34ac87119b966f92377
Luis Fernando <dev.lfchamorro@gmail.com>
2024-02-18 22:24:33 -0500

    className prop was added to SectionParallax component

--- src/components/SectionParallax/index.tsx
+++ src/components/SectionParallax/index.tsx
@@ -4,7 +4,11 @@ import { useInView } from "react-intersection-observer";
 import "./styles.scss";
 import { SectionParallaxProps } from "./types";
 
-const SectionParallax: React.FC<SectionParallaxProps> = ({ children, img }) => {
+const SectionParallax: React.FC<SectionParallaxProps> = ({
+  children,
+  className,
+  img,
+}) => {
   const [ref, inView] = useInView({
     triggerOnce: true,
     rootMargin: "-100px 0px",
@@ -13,7 +17,7 @@ const SectionParallax: React.FC<SectionParallaxProps> = ({ children, img }) => {
   const animateCssClass = inView ? "animate__animated animate__backInUp" : "";
 
   return (
-    <div className="section-parallax-container" ref={ref}>
+    <div className={`section-parallax-container ${className}`} ref={ref}>
       <div className="parallax-bg" style={{ backgroundImage: `url(${img})` }} />
       <div className={`parallax-content ${animateCssClass}`}>{children}</div>
     </div>
--- src/components/SectionParallax/types/section-parallax-props.ts
+++ src/components/SectionParallax/types/section-parallax-props.ts
@@ -2,5 +2,6 @@ import { ReactNode } from "react";
 
 export type SectionParallaxProps = {
   children?: ReactNode;
+  className?: string;
   img: string;
 };
