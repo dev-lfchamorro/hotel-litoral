ab35bb048f83911875558669b564298b627d64e2
Luis Fernando <dev.lfchamorro@gmail.com>
2024-02-20 22:26:08 -0500

    Design was finished for a TestimonialCard component

--- src/components/TestimonialCard/types/testimonial-card-props.ts
+++ src/components/TestimonialCard/types/testimonial-card-props.ts
@@ -1,9 +1,9 @@
 export type TestimonialCardProps = {
+  author: string;
   avatar: string;
   country?: string;
   description?: string;
   gender?: "female" | "male";
   starRating: number;
   title: string;
-  author: string;
 };
