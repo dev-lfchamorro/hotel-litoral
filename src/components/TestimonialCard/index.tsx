7a087013da344a6ec790df3be0c802c130104023
Luis Fernando <dev.lfchamorro@gmail.com>
2024-02-20 21:22:35 -0500

    TestimonialCard component was created and starRating logic was implement

---
+++ src/components/TestimonialCard/index.tsx
@@ -0,0 +1,40 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { getImgNameByPath } from "../../helpers";
+import "./styles.scss";
+import { TestimonialCardProps } from "./types";
+import { ReactComponent as IconStartFill } from "../../assets/icons/star-fill.svg";
+import { ReactComponent as IconStart } from "../../assets/icons/star.svg";
+
+const TestimonialCard: React.FC<TestimonialCardProps> = ({
+  avatar,
+  country,
+  description,
+  starRating,
+  title,
+  author,
+}) => {
+  return (
+    <motion.div className="testimonial-item">
+      <span className="star-rating">
+        {Array.from({ length: 5 }, (_, idx) => {
+          return idx + 1 > starRating ? (
+            <IconStart className="star-icon" />
+          ) : (
+            <IconStartFill className="star-icon" />
+          );
+        })}
+      </span>
+      <span className="title">{title}</span>
+      <span className="description">{description}</span>
+      <div className="avatar">
+        <img src={avatar} alt={getImgNameByPath(avatar)} />
+      </div>
+      <span className="author">
+        {author}, {country}
+      </span>
+    </motion.div>
+  );
+};
+
+export default TestimonialCard;
---
+++ src/components/TestimonialCard/types/testimonial-card-props.ts
@@ -0,0 +1,9 @@
+export type TestimonialCardProps = {
+  avatar: string;
+  country?: string;
+  description?: string;
+  gender?: "female" | "male";
+  starRating: number;
+  title: string;
+  author: string;
+};
