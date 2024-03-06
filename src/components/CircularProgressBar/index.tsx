cb4f51da4ff0fc442fa2e5e9a2b6ccd84fa88c11
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-05 21:42:35 -0500

    react-intersection-observer was implemented to detect the scroll

--- src/components/CircularProgressBar/index.tsx
+++ src/components/CircularProgressBar/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import { EColors, EMainColors } from "../../enums";
 import "./styles.scss";
 import { CircularProgressBarProps } from "./types";
@@ -10,6 +11,10 @@ const CircularProgressBar: React.FC<CircularProgressBarProps> = ({
 }) => {
   const [progressStartValue, setProgressStartValue] = useState(0);
 
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
   const progressValue = `${progressStartValue}%`;
   const progressEndValue = endValue;
 
@@ -20,27 +25,33 @@ const CircularProgressBar: React.FC<CircularProgressBarProps> = ({
   };
 
   useEffect(() => {
-    const progress = setInterval(() => {
-      setProgressStartValue((prevValue) => {
-        const nextValue = prevValue + 1;
+    if (inView) {
+      const progress = setInterval(() => {
+        setProgressStartValue((prevValue) => {
+          const nextValue = prevValue + 1;
 
-        if (nextValue <= progressEndValue) {
-          return nextValue;
-        } else {
-          clearInterval(progress);
-          return prevValue;
-        }
-      });
-    }, speed);
+          if (nextValue <= progressEndValue) {
+            return nextValue;
+          } else {
+            clearInterval(progress);
+            return prevValue;
+          }
+        });
+      }, speed);
 
-    return () => {
-      clearInterval(progress);
-    };
-  }, [speed, progressEndValue]);
+      return () => {
+        clearInterval(progress);
+      };
+    }
+  }, [speed, progressEndValue, inView]);
 
   return (
     <div className="circular-progress-container">
-      <div className="circular-progress" style={circularProgressStyle}>
+      <div
+        className="circular-progress"
+        ref={ref}
+        style={circularProgressStyle}
+      >
         <div className="circular-progress-content">
           <span className="progress-value">{progressValue}</span>
           <span className="text">{text}</span>
