4a6601383fcc40e670d7d402daabab2c436f96a4
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-10 19:55:22 -0500

    Change for static content on Footer component was implemented

--- src/components/common/Footer/constants/important-info.ts
+++ src/components/common/Footer/constants/important-info.ts
@@ -1,7 +1,7 @@
 import { ListItemType } from "../../../../types";
 
 export const ImportantInfo: ListItemType[] = [
-  { textItem: "Parcelamos em até 3 vezes sem juros." },
+  { textItem: "Parcelamos em até 2 vezes sem juros." },
   { textItem: "Crianças até 7 anos não pagam." },
   {
     textItem: "Os preços podem mudar em feriados e dias com eventos.",
