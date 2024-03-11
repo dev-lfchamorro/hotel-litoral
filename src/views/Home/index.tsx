d7d4b90470b8f07512b1bc5393b96ec05cc8e303
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-10 19:56:21 -0500

    CoverContent component was implemented on Home component

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { ReactComponent as IconBeachBed } from "../../assets/icons/beach-bed.svg";
 import { ReactComponent as IconBed } from "../../assets/icons/bed.svg";
 import { ReactComponent as IconHotel } from "../../assets/icons/hotel.svg";
+import ImgWhatsApp from "../../assets/icons/whatsapp.svg";
 import ImgSection1 from "../../assets/img/gallery/main-facade.jpeg";
+import ImgReception from "../../assets/img/gallery/reception.jpeg";
+import Button from "../../components/Button";
+import CoverContent from "../../components/CoverContent";
 import Feature from "../../components/Feature";
 import ImgCarousel from "../../components/ImgCarousel";
 import SectionImageText from "../../components/SectionImageText";
@@ -11,6 +15,7 @@ import Slider from "../../components/Slider";
 import TextContent from "../../components/TextContent";
 import Footer from "../../components/common/Footer";
 import Header from "../../components/common/Header";
+import { staticValues } from "../../constants";
 import { SectionsText } from "../../types";
 import { features, roomCardSlider } from "./constants";
 import "./styles.scss";
@@ -21,7 +26,21 @@ const Home: React.FC = () => {
   return (
     <div className="home-container">
       <Header />
+
+      <CoverContent bgImg={ImgReception}>
+        <div className="cover-content">
+          {staticValues.coverText}
+          <Button
+            className="cover-content-btn"
+            text={staticValues.whatsApp}
+            iconPath={ImgWhatsApp}
+            textSize={20}
+          />
+        </div>
+      </CoverContent>
+
       <Feature features={features} />
+
       <ImgCarousel />
 
       <SectionImageText
