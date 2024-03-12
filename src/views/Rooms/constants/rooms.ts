1840397ffd96a49e492e78dab9f4f7cd4df07949
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-11 21:37:25 -0500

    Constant to manage content for Rooms was created

---
+++ src/views/Rooms/constants/rooms.ts
@@ -0,0 +1,14 @@
+import { RoomCard } from "../../../types";
+import ImgRoom1 from "../../../assets/img/gallery/room-1.jpeg";
+import ImgRoom2 from "../../../assets/img/gallery/room-2.jpeg";
+import ImgRoom3 from "../../../assets/img/gallery/room-3.jpeg";
+import ImgRoom4 from "../../../assets/img/gallery/room-4.jpeg";
+import ImgRoom5 from "../../../assets/img/gallery/room-5.jpeg";
+
+export const roomsCardContent: RoomCard[] = [
+  { image: ImgRoom1, title: "Apto. Single" },
+  { image: ImgRoom2, title: "Apto. Duplo" },
+  { image: ImgRoom3, title: "Apto. Triplo" },
+  { image: ImgRoom4, title: "Apto. Quádruplo" },
+  { image: ImgRoom5, title: "Apto. Quintuplo" },
+];
