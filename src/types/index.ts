1265f670861a61b7a3f80ff740fab6b10ce57629
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-11 21:40:12 -0500

    RoomCard type was created

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export * from "./list-item-type";
 export * from "./menu-options";
+export * from "./room-card";
 export * from "./routes-config";
 export * from "./section-text";
 export * from "./sections-content-page";
