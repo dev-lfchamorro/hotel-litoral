253f9313edd9c5b7c9a79373abf24fc139d6ef9d
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-28 14:17:41 -0500

    WhatsApp contact button was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React, { Suspense } from "react";
 import "./App.scss";
+import IconWhatsApp from "./assets/icons/whatsapp-color.svg";
+import FixedContent from "./components/FixedContent";
+import { whatsAppLink } from "./constants";
 import AppRouter from "./navigation/AppRouter";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Suspense>
+        <FixedContent>
+          <a
+            className="whatsapp-fixed"
+            href={whatsAppLink()}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={IconWhatsApp} alt="Whatsapp" loading="lazy" />
+          </a>
+        </FixedContent>
+
         <AppRouter />
       </Suspense>
     </div>
