fe8dedbbabc20150da0ea93d1614f5ec494d74f3
Luis Fernando <dev.lfchamorro@gmail.com>
2024-04-22 21:59:21 -0500

    :id param was removed from base URL

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -2,11 +2,11 @@ import { Endpoints } from "../types";
 
 const endpoints: Endpoints = {
   contacts: {
-    create: "/contacts/create-contact",
-    delete: "/contacts/delete-contact/:id",
-    get: "/contacts/get-contacts",
-    getById: "/contacts/get-contacts/:id",
-    update: "/contacts/update-contact",
+    create: "/contacts/create-contact/",
+    delete: "/contacts/delete-contact/",
+    get: "/contacts/get-contacts/",
+    getById: "/contacts/get-contacts/",
+    update: "/contacts/update-contact/",
   },
 };
 
