1e0559ad5cfe963932405927dc2723fb3f303c6b
Luis Fernando <dev.lfchamorro@gmail.com>
2024-04-29 21:12:10 -0500

    Loader component was created

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+
+const Loader = () => {
+  return (
+    <section className="dots-container">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </section>
+  );
+};
+
+export default Loader;
